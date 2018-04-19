3cf54de2a5b2f707e524cf48a1627bd59083006d
Keda87 <adiyatmubarak@gmail.com>
2018-04-19 22:54:29 +0700

    Make round robin worker smarter. By not giving a new messsage if still processing the message and throw to another free worker instead

--- worker.py
+++ worker.py
@@ -15,6 +15,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback, queue='task_queue')
 
 print('  [*] Waiting for messages. To exit press CTRL+C')
